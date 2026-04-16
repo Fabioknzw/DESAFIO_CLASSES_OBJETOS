be06f717584166a1cf37e612623df63e7df6ceec
Fabioknzw <fabiopsilveira90@gmail.com>
2026-04-16 16:18:46 -0300

    ultima versão do desafio 3

--- desafio_escrevendo_Classe_superHeroi.js
+++ desafio_escrevendo_Classe_superHeroi.js
@@ -1,11 +1,41 @@
-class ClasseHeroi{
-    constructor(nome, idade, tipo){
-        this.nome = nome
-        this.idade = idade
-        this.tipo = tipo
+class heroi{
+    constructor(nome,idade,tipo){
+        this.nome=nome
+        this.idade=idade
+        this.tipo=tipo
     }
+
+    atacar(){
+        
+            
+        let ataque= ""
+        if (this.tipo === "guerreiro"){
+            ataque = "espada"
+        }
+
+        else if (this.tipo === "mago"){
+            ataque = "magia"
+        }
+
+        else if (this.tipo === "monge"){
+            ataque = "Artes marciais"
+        }
+
+        else if (this.tipo === "Ninja"){
+            ataque= "shuriken"
+        }
+        console.log(` o ${this.tipo} atacou usando ${ataque} `)
+        
+    }
+
 
 }
 
-let classeGuerreiro = new ClasseHeroi("fabio", "35", "Guerreiro")
-console.log(classeGuerreiro.tipo)
+let superHeroi = new heroi("fabio", 35, "mago")
+superHeroi.atacar()
+
+
+//se mago -> no ataque exibir (usou magia)
+//se guerreiro -> no ataque exibir (usou espada)
+//se monge -> no ataque exibir (usou artes marciais)
+//se ninja -> no ataque exibir (usou shuriken)
